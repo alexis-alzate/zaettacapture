0ef80a721b7d06d5a1f09d7f3ca613df113a5ea4
alexis <alexismejor10@hotmail.es>
2026-08-02 18:48:27 -0500

    nueva rama para estudiar

--- ZAETTA_CAPTURE_NATIVE/App/Program.cs
+++ ZAETTA_CAPTURE_NATIVE/App/Program.cs
@@ -1,15 +1,15 @@
-using System;
-using System.Threading;
-using System.Windows.Forms;
+using System; // permite usar exeptión para manejo de errores, ayuda a capturar el error
+using System.Threading; //control de hilos/sincronización Mutex. evita abrir 2 instanacias
+using System.Windows.Forms; //crear y ejecutar la app visual de windows forms
 
-namespace ZaettaCaptureNative
+namespace ZaettaCaptureNative// forma de organizar el código, como una carpteta logica donde viven las clases del proyecto
 {
     internal static class Program
     {
         [STAThread]
         private static void Main()
         {
-            bool createdNew = false;
+            bool createdNew = false; //candado ya existe por lo tanto debe cerrar la instancia, o no se abre
             Mutex singleInstance = null;
 
             try
